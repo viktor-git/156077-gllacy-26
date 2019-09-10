0a96aa682671119afbcd199ad4540ed6d7ce87b0
viktor-git <52155112+viktor-git@users.noreply.github.com>
2019-09-10 22:25:56 +0300

    Слайдер
    Переделан на добавление/удаление классов

--- js/js_main.js
+++ js/js_main.js
@@ -6,23 +6,23 @@ main.addEventListener ('click', function (evt) {
 	console.log(target);
 
 	if (target.className === 'first') {
-		main.style.backgroundImage = 'url("./img/Мороженко 1.png"), url("./img/Мороженко 2.png"), url("./img/Подсветка.png")';
-		main.style.backgroundColor = '#849D8F';
-		main.style.transition = 'background-image, background-color, ease-in, 1s';
-		text.textContent = 'Крем-брюле и пломбир с малиновым джемом';
+			main.classList.remove('second__theme');
+			main.classList.remove('third__theme');
+			main.classList.add('first__theme');
+			text.textContent = 'Крем-брюле и пломбир с малиновым джемом';
 	}
 
 	if (target.className === 'second') {
-		main.style.backgroundImage = 'url("./img/Мороженко 3.png"), url("./img/Мороженко 4.png"), url("./img/Подсветка 2.png")';
-		main.style.backgroundColor = '#8996A6';
-		main.style.transition = 'background-image, background-color, ease-in, 1s';
-		text.textContent = 'Шоколадный пломбир и лимонный сорбет';
+			main.classList.remove('third__theme');
+			main.classList.add('second__theme');
+			main.classList.add('trans');
+			text.textContent = 'Шоколадный пломбир и лимонный сорбет';
 	}
 
 	if (target.className === 'third') {
-		main.style.backgroundImage = 'url("./img/Мороженко 5.png"), url("./img/Мороженко 6.png"), url("./img/Подсветка 3.png")';
-		main.style.backgroundColor = '#9D8B84';
-		main.style.transition = 'background-image, background-color, ease-in, 1s';
-		text.textContent = 'Пломбир с помадкой и клубничный щербет';
+			main.classList.remove('second__theme');
+			main.classList.add('third__theme');
+			main.classList.add('trans');
+			text.textContent = 'Пломбир с помадкой и клубничный щербет';
 	}
 });
