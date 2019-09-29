cb0c6752208eb51e48edfae790e0cf3a5c4ef5dd
viktor-git <52155112+viktor-git@users.noreply.github.com>
2019-09-29 19:40:41 +0300

    Готова главная
    Осталось решить вопрос с превью при наведении, чтобы не сдвигало контент

--- js/js_main.js
+++ js/js_main.js
@@ -2,6 +2,11 @@
 
 main.addEventListener ('click', function (evt) {
 	let text = document.querySelector('.slider__title p');
+	let feedBack = document.querySelector('.feedback__form__container');
+	let overlay = document.createElement('div');
+	overlay.classList.add('overlay');
+	
+	overlay.style.height = document.documentElement.scrollHeight + 'px';
 	let target = evt.target;
 	console.log(target);
 
@@ -24,5 +29,22 @@ main.addEventListener ('click', function (evt) {
 			text.textContent = 'Пломбир с помадкой и клубничный щербет';
 	}
 
+	if (target.classList.contains('feedback')) {
+		feedBack.style.display = 'block';
+		let rect = feedBack.getBoundingClientRect();
+		feedBack.style.left = main.clientWidth / 3 + 'px';
+		feedBack.style.top =  rect.height - rect.y - 200 + 'px';
+		main.prepend(overlay);
+
+	}
+
+	if (target.className === 'close__btn') {
+		feedBack.style.display = 'none';
+		feedBack.style.top	= '0px';
+		document.querySelector('.overlay').remove();
+	}
+
 });
+
+
 
